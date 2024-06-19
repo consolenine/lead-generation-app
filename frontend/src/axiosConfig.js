5de97edaede6ebde04c3f0af76e7b8a288727d28
Shashank Deep <48642522+consolenine@users.noreply.github.com>
2024-06-19 13:29:48 +0530

    updated a axios base url

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/',
+    baseURL: 'http://159.89.4.217:8000/',
     withCredentials: true,
 });
 
